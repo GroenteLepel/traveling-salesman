68dc204c034f8bb2a18698b8d91440e80f4740dd
Ludo van Alst <l.vanalst@student.ru.nl>
2020-03-17 12:06:31 +0100

    Made the plot layout with final world and temperature vs average distance and temperature vs stdev distance

--- world.py
+++ world.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
+import matplotlib
 
 from city import City
 from tour import Tour
@@ -31,15 +31,20 @@ class World:
             plt.plot([from_city.x, to_city.x], [from_city.y, to_city.y])
 
         #plt.legend()
-        if title != None:
-            plt.savefig('../figures/{0:s}.png'.format(title))
+        if title:
+            plt.savefig('data/figures/{0:s}.jpg'.format(title))
         plt.show()
     
-    def show_withplots(self, temperature_list, avg_distance_list, stdev_distance_list):
-        fig = plt.figure(figsize=(8, 12))
-        outer = gridspec.GridSpec(2, 1, wspace=0.2, hspace=0.2)
-        
-        world_ax = plt.Subplot(fig, outer[0,0])
+    def show_withplots(self, temperature_list, avg_distance_list, stdev_distance_list, title=None):
+        matplotlib.rcParams.update({'font.size': 18})
+
+        fig = plt.figure(constrained_layout=False, figsize=(8, 12))
+        gs = fig.add_gridspec(nrows=3, ncols=2, wspace=0.1, hspace=0.1)
+
+        world_ax = fig.add_subplot(gs[0:-1, :])
+        avgdist_ax = fig.add_subplot(gs[2, 0])
+        stdevdist_ax = fig.add_subplot(gs[2, 1])
+
         for city in self.cities:
             world_ax.scatter(city.x, city.y, label="City {}".format(city.label))
 
@@ -49,18 +54,31 @@ class World:
                 to_city = self.tour.tour[0]
             else:
                 to_city = self.tour.tour[i+1]
-            world_ax.plot([from_city.x, to_city.x], [from_city.y, to_city.y])
-        fig.add_subplot(world_ax)
-        
-        inner = gridspec.GridSpecFromSubplotSpec(1, 2, subplot_spec=outer[1,0], wspace=0.2, hspace=0.3)
-        
-        ax0 = plt.Subplot(fig, inner[0], xscale='log', title='$AVG_{dist}$ vs temperature')
-        ax0.plot(temperature_list, avg_distance_list)
-        fig.add_subplot(ax0)
-        
-        ax1 = plt.Subplot(fig, inner[1], xscale='log', title='$\sigma_{dist}$ vs temperature')
-        ax1.plot(temperature_list, stdev_distance_list)
-        fig.add_subplot(ax1)
-        
-        plt.style.use('grayscale')
-        fig.savefig('test.png')
+            world_ax.plot([from_city.x, to_city.x], [from_city.y, to_city.y], color='black')
+        world_ax.set_xticklabels([])
+        world_ax.set_yticklabels([])
+        world_ax.xaxis.set_visible(False)
+        world_ax.yaxis.set_visible(False)
+
+
+        temperature_list.reverse()
+
+        avgdist_ax.plot(temperature_list, avg_distance_list, color='black')
+        # avgdist_ax.set_title('$AVG_{dist}$ vs temperature')
+        avgdist_ax.set_xscale('log')
+        avgdist_ax.set_xlabel('Temperature')
+        avgdist_ax.set_ylabel('Average distance')
+
+
+        stdevdist_ax.plot(temperature_list, stdev_distance_list, color='black')
+        # stdevdist_ax.set_title('$\sigma_{dist}$ vs temperature')
+        stdevdist_ax.set_xscale('log')
+        stdevdist_ax.set_xlabel('Temperature')
+        stdevdist_ax.set_ylabel('$\sigma$ distance')
+        stdevdist_ax.yaxis.set_label_position("right")
+        stdevdist_ax.yaxis.tick_right()
+
+        if title:
+            fig.savefig('data/figures/{0:s}'.format(title))
+
+        fig.show()
