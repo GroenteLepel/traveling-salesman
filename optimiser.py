1cfe0a1405b0206755be152dcdcc8bc2e5f5c682
Ludo van Alst <l.vanalst@student.ru.nl>
2020-03-17 12:05:16 +0100

    Only aesthetic changes

--- optimiser.py
+++ optimiser.py
@@ -7,25 +7,34 @@ from tour import Tour
 from world import World
 
 
-def sa_solution(world: World, n_mc_samples: int = -1, neighbourhood: int = 2,
-                init_temperature: float = -1, cooling_factor: float = 0.9, rising_mc: bool = True,
-                show_steps: bool = False, save_final_step: bool = False):
+def sa_solution(world: World,
+                n_mc_samples: int = -1,
+                neighbourhood: int = 2,
+                init_temperature: float = -1,
+                cooling_factor: float = 0.9,
+                rising_mc: bool = True,
+                show_steps: bool = False,
+                title = None):
+
     # Initial distance save for first calculation of diff_average
+    #
     init_distance = world.tour.distance
     
     # Initialise lists to use for plotting later
+    #
     temperature_list = []
     avg_distance_list = []
     stdev_distance_list = []
     
     # Initialise the multiplication factor for n_mc_samples
+    #
     if rising_mc:
         rising_mc_factor = cooling_factor**-1
     else:
         rising_mc_factor = 1
-        
     
     # Initialise the temperature
+    #
     if init_temperature < 0:
         init_temperature = world.n_cities / 8
     if n_mc_samples < 0:
@@ -33,15 +42,18 @@ def sa_solution(world: World, n_mc_samples: int = -1, neighbourhood: int = 2,
     temperature = init_temperature
     
     # Generate shuffled swap and reverse lists
+    #
     swap_list = generate_swaplist(world.n_cities, neighbourhood)
     reverse_list = generate_reverselist(world.n_cities)
     np.random.shuffle(swap_list)
     np.random.shuffle(reverse_list)
     
     # Calculate maximum amount of MC samples
+    #
     max_n_mc_samples = len(swap_list) + len(reverse_list)
 
     # Aesthetics
+    #
     print("==============")
     print("Starting simulated annealing with variables:")
     print(r"n_samples: {}, T_i: {}, f_c: {}, neighb.: {}".
@@ -59,6 +71,7 @@ def sa_solution(world: World, n_mc_samples: int = -1, neighbourhood: int = 2,
         new_sample_found = False
         
         # Go to the start of the lists since a new sample has just been found
+        #
         swap_list_counter = 0
         reverse_list_counter = 0
         
@@ -69,6 +82,7 @@ def sa_solution(world: World, n_mc_samples: int = -1, neighbourhood: int = 2,
             candidate_world = copy.deepcopy(world)
 
             # Choose between two methods for finding a new trial state
+            #
             if np.random.rand() < border:
                 swap = swap_list[swap_list_counter]
                 candidate_world.tour.swap_cities(swap)
@@ -81,22 +95,26 @@ def sa_solution(world: World, n_mc_samples: int = -1, neighbourhood: int = 2,
             new_distance = candidate_world.tour.distance
 
             # Check whether to acccept the new state
+            #
             if accept(current_distance, new_distance, temperature):
                 new_sample_found = True
                 world = copy.deepcopy(candidate_world)
                 
                 # Shuffle the lists and start all over again since previously
                 # skipped trials may now result in a lower energy.
+                #
                 np.random.shuffle(swap_list)
                 np.random.shuffle(reverse_list)
                 swap_list_counter = 0
                 reverse_list_counter = 0
                 
                 # Reset border
+                #
                 border = 0.5
             else:
                 # If all swaps or reverse directions are done, only the other
                 # method is used in the future.
+                #
                 if swap_list_counter == len(swap_list) and reverse_list_counter == len(reverse_list):
                     print("Tried all possible swaps and reverses.")
                     border = 0.5  # this does not matter since the loop will be stopped
@@ -118,6 +136,7 @@ def sa_solution(world: World, n_mc_samples: int = -1, neighbourhood: int = 2,
         stdev_distance_list.append(np.std(epoch_distances))
 
         # Calculate the difference before and after the epoch
+        #
         if epochs == 0:
             diff_average = avg_distance_list[-1] - init_distance
         else:
@@ -128,13 +147,14 @@ def sa_solution(world: World, n_mc_samples: int = -1, neighbourhood: int = 2,
             trend = "--"
         else:
             trend = "/\\"
-        print("{0:3.3f}\t\t{1:d}\t\t{2:3.3f}  {3}".format(temperature, n_mc_samples, avg_distance_list[-1], trend))
+        print("\r", "{0:3.3f}\t\t\t{1:d}\t\t\t\t{2:3.3f}  {3}".format(temperature, n_mc_samples, avg_distance_list[-1], trend), end='')
 
         if show_steps:
             world.show()
             plt.pause(0.1)
 
         # Update temperature and the amount of MC samples
+        #
         temperature *= cooling_factor
         if temperature < 0.25:
             n_mc_samples = int(rising_mc_factor * n_mc_samples)
@@ -145,8 +165,8 @@ def sa_solution(world: World, n_mc_samples: int = -1, neighbourhood: int = 2,
 #        print(tour_cities)
         epochs += 1
         
-    if save_final_step:
-        world.show_withplots(temperature_list, avg_distance_list, stdev_distance_list)
+    if title:
+        world.show_withplots(temperature_list, avg_distance_list, stdev_distance_list, title=title)
 
     return world
 
--- run.py
+++ run.py
@@ -3,11 +3,10 @@ import optimiser
 
 my_world = World(50)
 optimised_world = optimiser.sa_solution(world = my_world,  
-                                        n_mc_samples = 100, 
+                                        n_mc_samples = 300,
                                         neighbourhood = 2,
-                                        init_temperature = 1, 
+                                        init_temperature = 1.0,
                                         cooling_factor = 0.92,
                                         rising_mc = False,
                                         show_steps = False,
-                                        save_final_step = True)
-optimised_world.show(title = '1')
+                                        title = 'plotje.jpg')
